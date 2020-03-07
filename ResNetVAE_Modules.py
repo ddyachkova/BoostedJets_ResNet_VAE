a078e5db5beb1febf84e19748fed77de733e2e10
ddyachkova <darya.dyachkova@minerva.kgi.edu>
2020-03-07 16:51:19 +0800

    updatd the modules file

--- ResNetVAE_Modules.py
+++ ResNetVAE_Modules.py
@@ -61,14 +61,17 @@ class ParquetDataset(Dataset):
         data = self.parquet.read_row_group(index, columns=self.cols).to_pydict()
         data['X_jets'] = np.float32(data['X_jets'][0]) 
         # Preprocessing
-        data['X_jets'] = data['X_jets'][:, 20:105, 20:105]
+        #data['X_jets'] = data['X_jets'][:, 20:105, 20:105]
         data['X_jets'][data['X_jets'] < 1.e-3] = 0. # Zero-Suppression
         #scaling_factors = [40, 10, 250]
         #scaling_factors = [60, 5, 360]
         #scaling_factors = [360, 5, 60]
         #scaling_factors = [90, 1, 12]
-        scaling_factors = [80, 10, 30]
-
+        #scaling_factors = [80, 10, 300]
+        #scaling_factors = [23, 1.5, 2.3]
+        #scaling_factors = [120, 20, 30]
+        #scaling_factors = [240, 14, 4]
+        scaling_factors = [35, 3, 3]
         for i in range(3):
             if data['X_jets'][i, :, :].sum() != 0.0:
                 data['X_jets'][i, :, :] = data['X_jets'][i, :, :]*scaling_factors[i] # To standardize
@@ -107,11 +110,21 @@ def calc_loss(n_channels, X, Xreco):
     #sc_fact_arr = [4, 1, 25]
     #sc_fact_arr = [12, 1, 90]
     #sc_fact_arr = [90, 1, 12]
-    sc_fact_arr = [90, 100, 12]
+    #sc_fact_arr = [90, 100, 12]
+
+    #n_channels = 3
+    
     #mse = lambda X, Xreco, n, sc_fact: sc_fact * F.mse_loss(Xreco[:, n, :, :], X[:, n, :, :])
+    mse_total = lambda X, Xreco, n: F.mse_loss(Xreco[:, n, :, :], X[:, n, :, :])
+    mse_ind = lambda X, Xreco: F.mse_loss(Xreco, X)
+
+    #mse = lambda X, Xreco, n, sc_fact: sc_fact * F.l1_loss(Xreco[:, n, :, :], X[:, n, :, :])
+
     #return sum(map(partial(mse, X, Xreco), range(n_channels), sc_fact_arr)) 
-    mse = lambda X, Xreco, n: F.mse_loss(Xreco[:, n, :, :], X[:, n, :, :])
-    return sum(map(partial(mse, X, Xreco), range(n_channels))) 
+    if n_channels == 3:
+        return sum(map(partial(mse_total, X, Xreco), range(n_channels))) 
+    else: 
+        return mse_ind(X, Xreco) 
 
 def plot_orig_reco(X, Xreco, imgs, name, epoch, i, len_train_loader, loss):
     if imgs:
@@ -121,7 +134,8 @@ def plot_orig_reco(X, Xreco, imgs, name, epoch, i, len_train_loader, loss):
             raise ZeroTensorPrediction
         print(' >> AE-reco image:', name)
         plot_img(Xreco.detach().cpu().numpy().reshape(85, 85))
-    res = '%d: (%d/%d) Train loss:%f, Emax: %f, Erecomax: %f'%(epoch, i, len_train_loader, loss.item(), X.max(), Xreco.max())
+        ind_loss = calc_loss(1, X, Xreco)
+    res = '%d: (%d/%d) Total train loss:%f, Ind train loss: %f, Emax: %f, Erecomax: %f'%(epoch, i, len_train_loader, loss.item(), ind_loss.item(),  X.max(), Xreco.max())
     return res
 
 
@@ -130,57 +144,68 @@ def print_intermadiate_results(i, X, X_ind, Xreco, name, epoch, loss, len_train_
         if imgs:
             print (plot_orig_reco(X[0], Xreco[0], imgs, name, epoch, i, len_train_loader, loss))
     else:
-        for i in range(3):
-            print (plot_orig_reco(X[0][i], Xreco[0][0], imgs, name, epoch, i, len_train_loader, loss))
+        for j in range(3):
+            print (plot_orig_reco(X[0][j], Xreco[0][j], imgs, name, epoch, i, len_train_loader, loss))
 
             
 def calc_emd(img1, img2): 
     return emd(img1, img2, R=1.0, norm=False, beta=1.0, measure='euclidean', coords='hadronic', return_flow=False, gdim=None, mask=False, n_iter_max=100000, periodic_phi=False, phi_col=2, empty_policy='error')
 
 def calc_emd_reference(X, ind):
-    sample = X[:, ind, :, :].reshape(X.shape[0], X.shape[2], X.shape[3])
-    avg = np.average(sample,axis=0)
+#     sample = X[:, ind, :, :].reshape(X.shape[0], X.shape[2], X.shape[3])
+#     avg = np.average(sample,axis=0)
+    avg = np.average(X,axis=0)
     emd_arr = []
     for i in range(X.shape[0]):
-        emd_arr = np.append(emd_arr, calc_emd(avg, sample[i]))
-    return sample[emd_arr.argmin()]
+        #emd_arr = np.append(emd_arr, calc_emd(avg, sample[i]))
+        emd_arr = np.append(emd_arr, calc_emd(avg, X[i]))
+
+    #return sample[emd_arr.argmin()]
+    return X[emd_arr.argmin()]
 
 def calc_emd_vals(X, X_ind):
     reference = calc_emd_reference(X, X_ind)
-    X_sample = X.cpu().numpy()[:, X_ind, :, :].reshape(X.shape[0], X.shape[2], X.shape[3])
-    emd_vals = list(map(functools.partial(calc_emd, reference), X_sample))
+    emd_vals = list(map(functools.partial(calc_emd, reference), X))
     return emd_vals
 
+def eval_losses(X, Xreco, X_ind):
+    ind_loss = list(map(lambda x, xreco: np.sum((x - xreco) ** 2)/float(x.shape[0] * x.shape[1]), X, Xreco))
+    emd_vals = calc_emd_vals(X, X_ind)
+    return ind_loss, emd_vals
+
+def get_X(data, X_ind):
+    if X_ind != 3:
+        X = data['X_jets'][:, X_ind, :, :].reshape(data['X_jets'][:, X_ind, :, :].shape[0],  1, 85, 85).cuda()
+    else: 
+        X = data['X_jets'].cuda()
+    return X
+
 def do_eval(resnet, val_loader, epoch, X_ind, n_channels, idx=0):
-    loss_ = []
-    emd_vals = []
-    ind_loss = []
+    loss_, emd_vals_ind_loss = [], []
     a = time()
     monitor_step = 10
     for i, data in enumerate(val_loader):
         if i % monitor_step == 0:
             print (i)
-        if X_ind != 3:
-            X = data['X_jets'][:, X_ind, :, :].reshape(data['X_jets'][:, X_ind, :, :].shape[0],  1, 85, 85).cuda()
-            emd_vals = calc_emd_vals(X, X_ind)
-            ind_loss = list(map(lambda x, xreco: np.sum((x - xreco) ** 2)/float(x.shape[0] * x.shape[1]), X.cpu().numpy()[:, X_ind, :, :], Xreco.detach().cpu().numpy()[:, X_ind, :, :]))
-        else: 
-            X = data['X_jets'].cuda()
-            for i in range(3):
-                emd_vals.append(calc_emd_vals(X, i))
-                ind_loss.append(list(map(lambda x, xreco: np.sum((x - xreco) ** 2)/float(x.shape[0] * x.shape[1]), X.cpu().numpy()[:, i, :, :], Xreco.detach().cpu().numpy()[:, i, :, :])))
+        X = get_X(data, X_ind)
         Xreco = resnet(X)
-        X = X[...,1:-1,:]
-        Xreco = Xreco[...,1:-1,:]
-        loss_.append((calc_loss(n_channels, X, Xreco)).tolist())
+        if X_ind != 3:
+            emd_vals_ind_loss = eval_losses(X.cpu().numpy()[:, X_ind, :, :], Xreco.detach().cpu().numpy()[:, X_ind, :, :], X_ind)
+        if X_ind == 3: 
+            emd_vals_ind_loss = list(map(partial(eval_losses, X.cpu().numpy()[:, X_ind-1, :, :], Xreco.detach().cpu().numpy()[:, X_ind-1, :, :]), range(0, X_ind)))
+        X, Xreco = X[...,1:-1,:], Xreco[...,1:-1,:]
+        loss_.append(calc_loss(n_channels, X, Xreco).tolist())
     loss_ = np.array(loss_)  
+    emd_vals, ind_loss = zip(*emd_vals_ind_loss)
     s = '%d: Val loss:%f, MAE: %f, N samples: %d in %f min'%(epoch, loss_.mean(), (np.sqrt(loss_)).mean(), len(loss_), (time() -a)/60.)
     print(s)
     return loss_, emd_vals, ind_loss
 
+
+
 def save(add_dir, obj, n, expt_name, epoch, resnet, optimizer, new_model, name, ind):
     filename = n + name + '_' + ind + '_epoch_' + str(epoch) + '.pkl'
-    directory = wdir + '\%s\%s'%(add_dir, expt_name) + filename
+    directory = wdir + '\%s\%s'%(add_dir, expt_name) + '\\' + filename
     if add_dir == 'MODELS':
         obj = {'model': resnet.state_dict(), 'optim': optimizer.state_dict()}
     torch.save(obj, directory)
@@ -210,50 +235,32 @@ def save_model(wdir, expt_name, epoch, resnet, optimizer, metrics1, metrics2, me
 def train(train_loader, val_loader, resblocks, n_epochs, name, batch_size, wdir, expt_name, imgs=False):
     name_mapping = {'Tracks': 0, 'ECAL': 1, 'HCAL': 2, 'ECAL HCAL Tracks': 3}
     channels_mapping = {'Tracks': 1, 'ECAL': 1, 'HCAL': 1,  'ECAL HCAL Tracks': 3}
-    X_ind = name_mapping[name]
-    n_channels = channels_mapping[name]
-    resnet, optimizer, scheduler = initialize(resblocks, n_channels, sch)    
-    epochs = n_epochs
-    monitor_step = 10
+    X_ind, n_channels, epochs, monitor_step = name_mapping[name], channels_mapping[name], n_epochs, 10
+    resnet, optimizer, scheduler = initialize(resblocks, n_channels, sch)
     resnet.train()
-    metrics = []
     a = time()
     for e in range(epochs):
         epoch = e+1
-        s = '>> Epoch %d <<<<<<<<'%(epoch)
-        print(s)
-
-        print(">> Training <<<<<<<<")
+        print('>>>>>>>Epoch %d'%(epoch))
+        print(">>>>>>> Training")
         for i, data in enumerate(train_loader):
-            if X_ind != 3:
-                X = data['X_jets'][:, X_ind, :, :].reshape(data['X_jets'][:, X_ind, :, :].shape[0], 1, 85, 85).cuda()
-            else: 
-                X = data['X_jets'].cuda()
-            optimizer.zero_grad()
+            X = get_X(data, X_ind)
             Xreco = resnet(X)
+            optimizer.zero_grad()
             loss = calc_loss(n_channels, X, Xreco)
             loss.backward()
             optimizer.step()
             if scheduler is not None:
                 scheduler.step()
             if i % monitor_step == 0:
-                #if i!= 0:
                 try:
                     print_intermadiate_results(i, X, X_ind, Xreco, name, epoch, loss, len(train_loader), imgs)
                 except ZeroTensorPrediction:
                     break
-                # else: 
-                #     print_intermadiate_results(i, X, X_ind, Xreco, name, epoch, loss, len(train_loader), imgs)
-        s = '%d: Train time:%.2f min in %d steps'%(epoch, (time() - a)/60, len(train_loader))
-        print(s)
+        print('%d: Train time:%.2f min in %d steps'%(epoch, (time() - a)/60, len(train_loader)))
         resnet.eval()
-        print(">> Validation: Good samples <<<<<<<<")
+        print(">>>>>>> Validating")
         _eval, emd_vals, ind_loss = do_eval(resnet, val_loader, epoch, X_ind, n_channels)
-        if e == 0: 
-            new_model= True
-            print ('new model')
-        else: 
-            new_model= False
-        save_model(wdir, expt_name, epoch, resnet, optimizer, _eval, emd_vals, ind_loss, new_model, name)
-        #metrics.append(_eval)
+        new_model = [True if e == 0 else False]
+        save_model(wdir, expt_name, epoch, resnet, optimizer, _eval, emd_vals, ind_loss, new_model[0], name)
 
